45727e217e9cf30362cd46b31834788f28fedc03
cr0sh <ska827@naver.com>
2016-02-16 01:12:03 +0900

    Revert "Level code cleanup"
    This reverts commit 347dec0626145120fbeb2b02da800c2acbac6fff.

--- level/format/dummy/provider.go
+++ level/format/dummy/provider.go
@@ -1,1 +1,94 @@
 package dummy
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/L7-MCPE/lav7/types"
+	"github.com/L7-MCPE/lav7/util/buffer"
+)
+
+type Provider struct {
+	Name string
+
+	ChunkMap   map[[2]int32]*types.Chunk
+	ChunkMutex *sync.Mutex
+	Generator  func(int32, int32) *types.Chunk
+}
+
+func (pv Provider) ChunkExists(cx, cz int32) bool {
+	_, ok := pv.ChunkMap[[2]int32{cx, cz}]
+	return ok
+}
+
+func (pv Provider) GetChunk(cx, cz int32, create bool) (chk *types.Chunk) {
+	pv.ChunkMutex.Lock()
+	defer pv.ChunkMutex.Unlock()
+	if c, ok := pv.ChunkMap[[2]int32{cx, cz}]; ok {
+		return c
+	}
+	if path, err := filepath.Abs("levels/" + pv.Name + "/" + strconv.Itoa(int(cx)) + "_" + strconv.Itoa(int(cz)) + ".raw"); err != nil {
+		goto crt
+	} else if _, err := os.Stat(path); os.IsNotExist(err) {
+		goto crt
+	} else {
+		f, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			goto crt
+		}
+		b := make([]byte, 83200)
+		_, err = f.Read(b)
+		if err != nil {
+			goto crt
+		}
+		chk = new(types.Chunk)
+		buf := buffer.FromBytes(b)
+		chk.Read(buf)
+		pv.SetChunk(cx, cz, false, chk)
+		return
+	}
+crt:
+	if create {
+		chk = pv.Generator(cx, cz)
+		pv.SetChunk(cx, cz, false, chk)
+		return
+	}
+	return
+}
+
+func (pv Provider) SetChunk(cx, cz int32, force bool, c *types.Chunk) {
+	pv.ChunkMutex.Lock()
+	defer pv.ChunkMutex.Unlock()
+	if _, ok := pv.ChunkMap[[2]int32{cx, cz}]; !force && ok {
+		panic("Tried to overwrite existing chunk!")
+	}
+	if c.Mutex() == nil {
+		panic("Nil mutex: chunk may have been uninitialized!")
+	}
+	pv.ChunkMap[[2]int32{cx, cz}] = c
+}
+
+func (pv Provider) Save(name string) error {
+	pv.ChunkMutex.Lock()
+	defer pv.ChunkMutex.Unlock()
+	for k, c := range pv.ChunkMap {
+		path, _ := filepath.Abs("levels/" + name + "/" + strconv.Itoa(int(k[0])) + "_" + strconv.Itoa(int(k[1])) + ".raw")
+		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+			log.Println("Error while creating dir:", err)
+		}
+		buf, err := c.Write()
+		if err != nil {
+			log.Println("Error while writing chunk to file:", err)
+			continue
+		}
+		if err := ioutil.WriteFile(path, buf.Done(), 0644); err != nil {
+			log.Println("Error while saving:", err)
+			continue
+		}
+	}
+	return nil
+}
--- level/level.go
+++ level/level.go
@@ -1,4 +1,11 @@
+// Package level implements MCPE world components and defines associated interfaces.
 package level
+
+import (
+	"log"
+
+	"github.com/L7-MCPE/lav7/types"
+)
 
 const (
 	DayTime     = 0
@@ -8,7 +15,22 @@ const (
 	FullTime    = 24000
 )
 
-// Face direction: (NOTE)
+type Level struct {
+	provider LevelProvider
+}
+
+func (lv *Level) Init(pv LevelProvider) {
+	lv.provider = pv
+}
+
+func (lv *Level) Provider() LevelProvider {
+	return lv.provider
+}
+
+// OnUseItem handles UseItemPacket and determines position to update block position.
+// Note: Value of x, y, z could be changed
+//
+// Face direction:
 //
 // 0: Down  (Y-)
 // 1: Up    (Y+)
@@ -16,3 +38,79 @@ const (
 // 3: South (Z+)
 // 4: West  (X-)
 // 5: East  (X+)
+func (lv *Level) OnUseItem(x, y, z *int32, face byte, item *types.Item) (canceled bool) {
+	log.Println("OnTouch:", x, y, z, face)
+	switch face {
+	case 0:
+		*y--
+	case 1:
+		*y++
+	case 2:
+		*z--
+	case 3:
+		*z++
+	case 4:
+		*x--
+	case 5:
+		*x++
+	}
+	if f := lv.GetBlock(*x, *y, *z); f == 0 {
+		log.Printf("SetBlock %d %d %d %v", *x, *y, *z, item.Block())
+		lv.Set(*x, *y, *z, item.Block())
+	} else {
+		log.Println("Block", f)
+	}
+	return
+}
+
+func (lv Level) GetBlock(x, y, z int32) byte {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	c.Mutex().RLock()
+	defer c.Mutex().RUnlock()
+	return c.GetBlock(byte(x&0xf), byte(y), byte(z&0xf))
+}
+
+func (lv *Level) SetBlock(x, y, z int32, b byte) {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	c.Mutex().Lock()
+	defer c.Mutex().Unlock()
+	c.SetBlock(byte(x&0x0f), byte(y), byte(z&0x0f), b)
+}
+
+func (lv Level) GetBlockMeta(x, y, z int32) byte {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	c.Mutex().RLock()
+	defer c.Mutex().RUnlock()
+	return c.GetBlockMeta(byte(x&0xf), byte(y), byte(z&0xf))
+}
+
+func (lv *Level) SetBlockMeta(x, y, z int32, b byte) {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	c.Mutex().Lock()
+	defer c.Mutex().Unlock()
+	c.SetBlockMeta(byte(x&0x0f), byte(y), byte(z&0x0f), b)
+}
+
+func (lv Level) Get(x, y, z int32) *types.Block {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	return &types.Block{
+		ID:   c.GetBlock(byte(x&0xf), byte(y&0xf), byte(z&0xf)),
+		Meta: c.GetBlockMeta(byte(x&0xf), byte(y&0xf), byte(z&0xf)),
+	}
+}
+
+func (lv Level) Set(x, y, z int32, block *types.Block) {
+	c := lv.Provider().GetChunk(x>>4, z>>4, true)
+	c.SetBlock(byte(x&0xf), byte(y&0xf), byte(z&0xf), block.ID)
+	c.SetBlockMeta(byte(x&0xf), byte(y&0xf), byte(z&0xf), block.Meta)
+}
+
+func (lv Level) GetTime() uint16 {
+	return 0
+}
+
+func (lv Level) SetTime(t uint16) {}
+
+func (lv Level) GetName() string {
+	return "Dummy"
+}
